147327dbd52c1c78c5d0de19fae6a53ddaa8c2c2
Max <33128710+vandenesseml@users.noreply.github.com>
2018-03-26 13:38:01 -0400

    fix R issues

--- Sprint1/app/src/main/java/e/vcu/quizly/StudentHomepage.java
+++ Sprint1/app/src/main/java/e/vcu/quizly/StudentHomepage.java
@@ -1,8 +1,16 @@
 package e.vcu.quizly;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by Max Vandenesse on 3/26/2018.
  */
 
-public class StudentHomepage {
+public class StudentHomepage extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.student_add_quiz);
+    }
 }

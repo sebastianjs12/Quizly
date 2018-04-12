8c48b5db39990cabf06e2cb05dfc9393daee67fd
Kevin Hall <hallkj4@mymail.vcu.edu>
2018-04-12 11:16:22 -0400

    THis should have DatabaseHelper up v2

--- Sprint1/app/src/main/java/e/vcu/quizly/DatabaseHelper.java
+++ Sprint1/app/src/main/java/e/vcu/quizly/DatabaseHelper.java
@@ -1,34 +1,68 @@
 package e.vcu.quizly;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class DatabaseHelper {
-
     Map<String, Quiz> quizzes = new HashMap<>();
+    //list of quizzes returned
+    final List<Quiz> quizList = new ArrayList<>();
+    Quiz send = null;
 
     final FirebaseDatabase database = FirebaseDatabase.getInstance();
     public DatabaseReference ref;
+    public DatabaseReference quizChild;
     public DatabaseHelper(){
-
         ref = database.getReference();
     }
 
 
 
     public void insertQuiz(Quiz q){
-        DatabaseReference quizChild = ref.child("quizzes").push();
-        String key = q.getKey();
-        quizzes.put(key, q);
+
+        quizChild = ref.child("quizzes").push();
         quizChild.setValue(quizzes);
     }
 
     public Quiz getQuiz(String key){
-        Quiz q= new Quiz();
-        return q;
+        ref.child("quizzes").addValueEventListener(new ValueEventListener() {
+            // THis method will be envoked anytime the data on the database changes
+            //Connect as soon as we connect to listener, so we get an inital snapshot of the database
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+                for (DataSnapshot child: children) {
+                    Quiz temp = child.getValue(Quiz.class);
+                    quizList.add(temp);
+                }
+
+            }
+            
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        for (Quiz q: quizList) {
+            if(q.getKey() == key){
+                this.send = q;
+            }
+
+        }
+        if(send == null){
+            return null;
+        }
+        else{
+            return send;
+        }
     }
     public Quiz setDueDate(int i, Quiz quiz){
         quiz.setDueDate(i);

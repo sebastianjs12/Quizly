fb594fb51f0c5be3b6f5e50614eb1dea8eba2dfe
Kevin Hall <hallkj4@mymail.vcu.edu>
2018-04-24 12:57:10 -0400

    Database helper can potentially pull

--- Sprint1/app/src/main/java/e/vcu/quizly/DatabaseHelper.java
+++ Sprint1/app/src/main/java/e/vcu/quizly/DatabaseHelper.java
@@ -1,5 +1,7 @@
 package e.vcu.quizly;
 
+import android.support.annotation.Nullable;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -12,63 +14,38 @@ import java.util.List;
 import java.util.Map;
 
 public class DatabaseHelper {
-    Map<String, Quiz> quizzes = new HashMap<>();
-    //list of quizzes returned
-    final List<Quiz> quizList = new ArrayList<>();
-    Quiz send = null;
-
-    final FirebaseDatabase database = FirebaseDatabase.getInstance();
-    public DatabaseReference ref;
-    public DatabaseReference quizChild;
-    public DatabaseHelper(){
-        ref = database.getReference();
+    public interface QuizReceivedCallback {
+        void onQuizRecieved(@Nullable Quiz q);
     }
 
+    private final DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
 
-
-    public void insertQuiz(Quiz q){
-
-        quizChild = ref.child("quizzes").push();
-        quizChild.setValue(quizzes);
+    public void addQuiz(Quiz newQuiz){
+        ref.child("quizzes").push().setValue(newQuiz);
     }
 
-    public void getQuiz(final String key){
-        Quiz temp;
-        ref.child("quizzes").addValueEventListener(new ValueEventListener() {
-            // THis method will be envoked anytime the data on the database changes
+    public void getQuiz(final String key, final QuizReceivedCallback callback){
+        ref.child("quizzes").addListenerForSingleValueEvent(new ValueEventListener() {
+            // This method will be envoked anytime the data on the database changes
             //Connect as soon as we connect to listener, so we get an inital snapshot of the database
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                Iterable<DataSnapshot> children = dataSnapshot.getChildren();
-                for (DataSnapshot child: children) {
-                    Quiz temp = child.getValue(Quiz.class);
-                    quizList.add(temp);
-                    for (Quiz quiz: quizList) {
-                        if(quiz.getKey() == key){
-                            temp = quiz;
-                            break;
-                        }
+                for (DataSnapshot child: dataSnapshot.getChildren()) {
+                    Quiz quiz = child.getValue(Quiz.class);
+                    if (quiz != null && quiz.getKey().equals(key)) {
+                        callback.onQuizRecieved(quiz);
+                        return;
                     }
                 }
+                callback.onQuizRecieved(null);
 
             }
+
             @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
+            public void onCancelled(DatabaseError databaseError) {}
         });
     }
-    public Quiz sendQuiz(Quiz q){
-        if(q == null){
-            return null;
-        }
-        else{
-            return q;
-        }
-    }
-    public Quiz setDueDate(int i, Quiz quiz){
-        //  quiz.setDueDate(i);
-        return quiz;
-    }
+
+    public void updateQuiz(Quiz q) {}
 
 }
--- Sprint1/app/src/main/java/e/vcu/quizly/StudentAddQuiz.java
+++ Sprint1/app/src/main/java/e/vcu/quizly/StudentAddQuiz.java
@@ -3,6 +3,7 @@ package e.vcu.quizly;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -19,6 +20,7 @@ import java.util.Date;
 
 public class StudentAddQuiz extends Activity {
     FirebaseAuth firebaseAuth;
+    DatabaseHelper helper;
 
     Quiz quiz;
     @Override
@@ -26,6 +28,7 @@ public class StudentAddQuiz extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.student_add_quiz);
         firebaseAuth = FirebaseAuth.getInstance();
+        helper = new DatabaseHelper();
 
         //test input
         //newQuiz.build();
@@ -36,31 +39,31 @@ public class StudentAddQuiz extends Activity {
     //Add Quiz
     public void clickAddQuiz(View v) {
         if (v.getId() == R.id.addQuiz) {
-            System.out.println(quiz.getQuizID()+"*******************");
             //gather username and password and store to strings
-            EditText ID =(EditText)findViewById(R.id.QuizIdInput);
-            String IDStr = ID.getText().toString();
+            EditText ID = (EditText) findViewById(R.id.QuizIdInput);
+            final String IDStr = ID.getText().toString();
+            //Checks data in firebase for Quizid
+            //TODO
+            //System.out.println(quiz.getQuizID()+"*******************");
+            //gather username and password and store to strings
             //print quiz ID
-            if(!IDStr.equals(quiz.getQuizID())){
-                Toast.makeText(this, "This quiz does not exist! Please enter a Quiz ID",
-                        Toast.LENGTH_SHORT).show();
-            }
-            //Checks data in firebase for QuizID
-            else {
-                if(new Date().before(quiz.getDueDate())){
-                    Intent i = new Intent(StudentAddQuiz.this, StudentQuestionTemp.class);
-                    startActivity(i);
+            helper.getQuiz(IDStr, new DatabaseHelper.QuizReceivedCallback() {
+                @Override
+                public void onQuizRecieved(Quiz quiz) {
+                    if (quiz == null) {
+                        Toast.makeText(getApplicationContext(), "This quiz does not exist! Please enter a Quiz ID",
+                                Toast.LENGTH_SHORT).show();
+                    } else {
+                        Intent i = new Intent(StudentAddQuiz.this, StudentQuestionTemp.class);
+                        i.putExtra("quiz", (Parcelable) quiz);
+                        startActivity(i);
+                    }
                 }
-                else {
-                    System.out.println(quiz.getDueDate());
-                    Toast.makeText(this, "This quiz has closed!",
-                            Toast.LENGTH_SHORT).show();
-
-                }
-            }
+            });
         }
     }
-    //Back To Homepage
+
+            //Back To Homepage
     public void clickBackToManage(View v) {
         if (v.getId() == R.id.StudentBack) {
             Intent i = new Intent(StudentAddQuiz.this, StudentHomepage.class);
